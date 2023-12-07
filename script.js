c267e7943abeae4bfc9313a6a2deb8f3b5646caf
Sergio Saldaña <ssaldana.dev@gmail.com>
2023-12-07 03:44:08 -0600

    Fixed a bug on the gallery

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-// Seleccionar el contenedor de la galería y las imágenes dentro de él
+// Seleccionar el contenedor de la galería y las imágenes dentro de él.
 const galleryContainer = document.querySelector(".gallery");
 const galleryImages = galleryContainer.querySelectorAll(".image");
 
-// Inicializar variables para el tamaño de la imagen, separación entre imágenes y distancia de movimiento
+// Inicializar variables para el tamaño de la imagen, separación entre imágenes y distancia de movimiento.
 let imageSize, imageSeparation, moveDistance;
 
-// Función para actualizar el tamaño de la imagen y la separación entre imágenes
+// Función para actualizar el tamaño de la imagen y su separación relacionadas con el tamaño de la galería.
 function updateImageProperties() {
-    imageSize = (galleryContainer.clientWidth / 100) * 30;
-    imageSeparation = ((galleryContainer.clientWidth / 100) * 10) / 6;
-    moveDistance = imageSeparation * 2 + imageSize;
+    if (galleryContainer.clientWidth > 850) {
+        imageSize = (galleryContainer.clientWidth / 100) * 20;
+        imageSeparation = ((galleryContainer.clientWidth / 100) * 20) / 8;
+        moveDistance = imageSeparation * 2 + imageSize;
+    } else {
+        imageSize = (galleryContainer.clientWidth / 100) * 30;
+        imageSeparation = ((galleryContainer.clientWidth / 100) * 10) / 6;
+        moveDistance = imageSeparation * 2 + imageSize;
+    }
 }
 
-// Función para ajustar el tamaño y la posición de la galería
+// Función para ajustar el tamaño de las imágenes y su separación dentro de la galería.
 function adjustGallery() {
     updateImageProperties();
     galleryContainer.style.height = `${imageSize}px`;
+    galleryContainer.scrollTo(moveDistance * currentPosition, 0);
 
-    // Iterar sobre todas las imágenes y aplicar estilos
+    // Iterar sobre todas las imágenes y aplicar estilos.
     for (const image of galleryImages) {
         image.style.margin = `0px ${imageSeparation}px 0px ${imageSeparation}px`;
         image.style.width = `${imageSize}px`;
@@ -25,28 +32,36 @@ function adjustGallery() {
     }
 }
 
-// Seleccionar los botones de navegación izquierda y derecha
+// Seleccionar los botones de navegación izquierda y derecha.
 const leftNavigationButton = document.querySelector(".left");
 const rightNavigationButton = document.querySelector(".right");
 
-// Agregar eventos de clic a los botones de navegación
+// Agregar eventos de clic a los botones de navegación.
 leftNavigationButton.addEventListener("click", moveLeft);
 rightNavigationButton.addEventListener("click", moveRight);
 
-// Inicializar variables para el número total de imágenes y la posición actual
+// Inicializar variables para el número total de imágenes y la posición actual.
 let totalImages = galleryImages.length;
 let currentPosition = 0;
 
-// Función para desplazar la galería hacia la derecha
+// Función para desplazar la galería hacia la derecha.
 function moveRight() {
-    if (currentPosition < totalImages - 3) {
-        updateImageProperties();
-        galleryContainer.scrollTo(moveDistance * (currentPosition + 1), 0);
-        currentPosition++;
+    if (galleryContainer.clientWidth > 850) {
+        if (currentPosition < totalImages - 4) {
+            updateImageProperties();
+            galleryContainer.scrollTo(moveDistance * (currentPosition + 1), 0);
+            currentPosition++;
+        }
+    } else {
+        if (currentPosition < totalImages - 3) {
+            updateImageProperties();
+            galleryContainer.scrollTo(moveDistance * (currentPosition + 1), 0);
+            currentPosition++;
+        }
     }
 }
 
-// Función para desplazar la galería hacia la izquierda
+// Función para desplazar la galería hacia la izquierda.
 function moveLeft() {
     if (currentPosition > 0) {
         updateImageProperties();
@@ -55,10 +70,18 @@ function moveLeft() {
     }
 }
 
-// Función para ajustar la galería al cargar la página
+// Función para ajustar la galería al cambiar el tamaño de la página.
+function adjustGalleryOnResize() {
+    adjustGallery();
+}
+
+// Añadir un evento que llame a la función cuando cambie el tamaño de la ventana.
+window.addEventListener("resize", adjustGalleryOnResize);
+
+// Función para ajustar la galería al cargar la página.
 function adjustGalleryOnLoad() {
     adjustGallery();
 }
 
-// Añadir un evento que llame a la función cuando se cargue la página
+// Añadir un evento que llame a la función cuando se cargue la página.
 window.addEventListener("load", adjustGalleryOnLoad);
